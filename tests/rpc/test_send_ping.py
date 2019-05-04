0021f6293a1a1c3a5884b7a84692a53cf27c8f1c
ATTY Lionel <lionel.atty@gmail.com>
2019-05-05 00:18:50 +0200

    CI: fix on tox.ini to work on alpine+grpc+py37 (Travis).

--- tests/rpc/test_send_ping.py
+++ tests/rpc/test_send_ping.py
@@ -8,7 +8,7 @@ import grpc_ping_pong.rpc.client.send_ping as send_ping
 def test_send_ping(core_rpc_stub, monkeypatch):
     with monkeypatch.context() as m:
         m.setenv('GRPC_PING_PONG_GRPC_HOST_AND_PORT',
-                 f'[::]:{pytest.insecure_port}')
+                 f'localhost:{pytest.insecure_port}')
         m.setattr(sys, 'argv',
                   ['send_ping.py',
                    '--message', 'test', '--delaySeconds', '0.142'])
